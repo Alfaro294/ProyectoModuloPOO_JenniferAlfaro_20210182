80414501c8be29046d7fdf40fa41f4a22f6a8b2a
Alfaro294 <jenniferalfaro0819@gmail.com>
2025-08-22 10:52:47 -0600

    Creación de paquete controller

---
+++ JenniferAlfaro_20210182/src/main/java/ProyectoModuloPOO_JenniferAlfaro_20210182/JenniferAlfaro_20210182/JenniferAlfaro20210182Application.java
@@ -0,0 +1,13 @@
+package ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenniferAlfaro20210182Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenniferAlfaro20210182Application.class, args);
+	}
+
+}
---
+++ JenniferAlfaro_20210182/src/test/java/ProyectoModuloPOO_JenniferAlfaro_20210182/JenniferAlfaro_20210182/JenniferAlfaro20210182ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenniferAlfaro20210182ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
