233b3b1b1139c4e4e8ee2713787eaf868ce24b2d
Alfaro294 <jenniferalfaro0819@gmail.com>
2025-08-22 13:48:20 -0600

    Cambios en controlador y services

--- JenniferAlfaro_20210182/src/main/java/ProyectoModuloPOO_JenniferAlfaro_20210182/JenniferAlfaro_20210182/Controller/ControllerLibros.java
+++ JenniferAlfaro_20210182/src/main/java/ProyectoModuloPOO_JenniferAlfaro_20210182/JenniferAlfaro_20210182/Controller/ControllerLibros.java
@@ -1,4 +1,36 @@
 package ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Controller;
 
+import ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Entities.LibrosEntity;
+import ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Models.DTO.LibrosDTO;
+import ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Services.ServiceLibros;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@RestController @RequestMapping("/api/libros")
 public class ControllerLibros {
+    @Autowired
+    private ServiceLibros serviceLibros;
+
+    @GetMapping("/getDataBooks")
+    private ResponseEntity <List<LibrosDTO>> getData(){
+        List<LibrosDTO> librosDTOS = serviceLibros.getAllBooks();
+
+        if (librosDTOS == null){
+            ResponseEntity.badRequest().body(Map.of(
+                    "Status", "No hay libros registrados"
+            ));
+        }
+        return ResponseEntity.ok(librosDTOS);
+    }
+    @PostMapping("/newBooks")
+    private ResponseEntity<Map<String, Object>>Insert
+
 }
--- JenniferAlfaro_20210182/src/main/java/ProyectoModuloPOO_JenniferAlfaro_20210182/JenniferAlfaro_20210182/Services/ServiceLibros.java
+++ JenniferAlfaro_20210182/src/main/java/ProyectoModuloPOO_JenniferAlfaro_20210182/JenniferAlfaro_20210182/Services/ServiceLibros.java
@@ -1,13 +1,17 @@
 package ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Services;
 
 import ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Entities.LibrosEntity;
+import ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Exceptions.ExceptionLibros;
 import ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Models.DTO.LibrosDTO;
 import ProyectoModuloPOO_JenniferAlfaro_20210182.JenniferAlfaro_20210182.Repositories.LibrosRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.Console;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service @Slf4j
 public class ServiceLibros {
@@ -18,6 +22,66 @@ public class ServiceLibros {
         List<LibrosEntity> libros = repo.findAll();
         return libros.stream()
                 .map(this::converirAllLibrosDTO)
-                
+                .collect(Collectors.toList());
+    }
+    public LibrosDTO insertBooks(@Valid LibrosDTO json){
+        if (json == null){
+            throw new IllegalArgumentException("La información no puede ser nula");
+        }
+        try{
+            LibrosEntity objData = convertirAEntity(json);
+            LibrosEntity libroGuardado = repo.save(objData);
+            return converirAllLibrosDTO (libroGuardado);
+        } catch (Exception e){
+            log.error("Error al registrar" + e.getMessage());
+            throw new ExceptionLibros("El libro no pudo ser registrado");
+        }
+    }
+    public LibrosDTO update(long id, @Valid LibrosDTO json){
+        LibrosEntity librosExistente = repo.findById(id).orElseThrow() -> new ExceptionLibros("Libro no encontrado");
+        //Actualización de campos
+        librosExistente.setTitulo(json.getTitulo());
+        librosExistente.setIsbn(json.getIsbn());
+        librosExistente.setAño_publicacion(json.getAño_publicacion());
+        librosExistente.setAutor_id(json.getAutor_id());
+
+        //ACtualización del registro
+        LibrosEntity libroActualizado = repo.save(librosExistente);
+
+        //Convertir a DTO
+        return converirAllLibrosDTO(libroActualizado);
+    }
+
+    public boolean delete (long id){
+        LibrosEntity existencia = repo.findById(id).orElse(null);
+
+        if (existencia != null){
+            repo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    private LibrosDTO converirAllLibrosDTO(LibrosEntity objEntity){
+        LibrosDTO dtoL = new LibrosDTO();
+        dtoL.setId(objEntity.getId());
+        dtoL.setTitulo(objEntity.getTitulo());
+        dtoL.setIsbn(objEntity.getIsbn());
+        dtoL.setAño_publicacion(objEntity.getAño_publicacion());
+        dtoL.setGenero(objEntity.getGenero());
+        dtoL.setAutor_id(objEntity.getAutor_id());
+
+        return dtoL;
+    }
+
+    private LibrosEntity convertirAEntity (@Valid LibrosDTO json){
+        LibrosEntity entity = new LibrosEntity();
+        entity.setTitulo(json.getTitulo());
+        entity.setIsbn(json.getIsbn());
+        entity.setAño_publicacion(json.getAño_publicacion());
+        entity.setGenero(json.getGenero());
+        entity.setAutor_id(json.getAutor_id());
+
+        return entity;
     }
 }
